81cd1c563aef43122f68ec57f452d60a52ebb718
mxz96102 <mxz96102@qq.com>
2017-02-25 00:15:15 +0800

    75% complete

---
+++ src/components/load.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="load animated">
+    <div>
+     <div>
+     </div>
+     <div>Loading...</div>
+    </div>
+  </div>
+</template>
+
+<script>
+require('../assets/animate.css')
+
+export default {
+  name: 'join',
+  data () {
+    return {
+    }
+  },
+  mounted(){
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
